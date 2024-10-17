91163d8fb6a592178cfcca37fe70d9b2a3c6b700
HP <arunagiri9062@mountzion.ac.in>
2024-10-17 14:46:01 +0530

    Dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './Dashboard.css'
 import Navbar from '../../Components/Navbar/Navbar'
 import evm from '../../assets/evm.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import model from '../../assets/model.jpg';
+import model2 from '../../assets/model2.jpg';
 
 const Dashboard = () => {
     return (
@@ -18,8 +22,51 @@ const Dashboard = () => {
                     </div>
                 </div>
             </div>
+            {/* new features section */}
+            <div className="features">
+                <div className="content">
+                    <div className="left">
+                        <img src={model} alt="" />
+                    </div>
+                    <div className="center"></div>
+                    <div className="right">
+                        <h2>Whats New</h2>
+                        <div className="list">
+                            <div className="item">
+                                <parseFloat><strong>Blockchain-Integrated Security</strong></parseFloat>
+                                <FontAwesomeIcon icon={faChevronDown} />
+                            </div>
+                            <div className="item">
+                                <p><strong>Real-Time Monitoring with Admin Panel</strong></p>
+                                <FontAwesomeIcon icon={faChevronDown} />
+                            </div>
+                            <div className="item">
+                                <p><strong>Crypto Cloud Storage</strong></p>
+                                <FontAwesomeIcon icon={faChevronDown} />
+                            </div>
+                            <div className="item">
+                                <p><strong>User-Friendly Voting Interface</strong></p>
+                                <FontAwesomeIcon icon={faChevronDown} />
+                            </div>
+                            <div className="item">
+                                <p><strong>Configurable via Application Interface</strong></p>
+                                <FontAwesomeIcon icon={faChevronDown} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="product">
+                <div className="content">
+                    <div className="left">
+                        <h2>Product</h2>
+                    </div>
+                    <div className="right">
+                        <img src={model2} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
-
 export default Dashboard
